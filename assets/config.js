9e70e943f8eea5dfca74c5b3e3d43ba6a53beb68
Weic <i@weic.me>
2017-09-12 17:01:21 +0800

    Release......

---
+++ assets/config.js
@@ -0,0 +1,30 @@
+//
+// 此文件为Apex主题配置文件，使用方法请查看  https://weic.me/themes-life
+//
+
+
+// 站点信息
+var siteInfo = {
+      icp: "",                 //I CP备案号，没有可不填
+      alipay: "http://url.com/a.jpg",            // 文章打赏用支付宝二维码URL
+      wechatpay: "http://url.com/a.jpg",    // 文章打赏用微信支付二维码URL
+      tenpay: "http://url.com/a.jpg",          // 文章打赏用财付通(QQ)二维码URL
+      github: "https://github.com",
+      weibo: "",
+      email: "",
+      twitter: "",
+      facebook: "",
+      rss: ""
+};
+
+// LeanCloud配置，用于Valine评论模块和LeanStatistics阅读次数统计模块
+var leanCloud = {
+      App_ID:"",
+      App_Key:"",
+      VClass:""              // 这个是LeanStatistics阅读次数统计模块的class，新建一个叫Counter的class（https://weic.me/LeanStatistics.js/）
+};
+
+// Disqus配置
+var disqus = {
+      name:"",
+};
